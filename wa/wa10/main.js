55571bce1c241c07cba9d7d3e4c0df201930ec38
KaiHansen22 <166645695+KaiHansen22@users.noreply.github.com>
2024-06-30 12:25:09 -0600

    Update main.js

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -6,11 +6,11 @@ function randomValueFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "On a chilly day of 45 fahrenheit, :insertx: decided to go for a jog. As they reached :inserty:, they couldn't believe their eyes and suddenly :insertz:. Bob witnessed everything, but was not startled — :insertx: weighs 150 pounds, and it was an unusual sight.";
 
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const insertX = ['Jack the Giant', 'Super Grandma', 'Captain Marvel'];
+const insertY = ['the haunted house', 'the spooky forest', 'the mysterious cave'];
+const insertZ = ['vanished into thin air', 'started dancing wildly', 'transformed into a frog'];
 
 randomize.addEventListener('click', result);
 
@@ -29,10 +29,10 @@ function result() {
     }
 
     if (document.getElementById("uk").checked) {
-        const weight = Math.round(300 * 0.0714286) + ' stone';
-        const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
-        newStory = newStory.replace('300 pounds', weight);
-        newStory = newStory.replace('94 fahrenheit', temperature);
+        const weight = Math.round(150 * 0.0714286) + ' stone';
+        const temperature = Math.round((45 - 32) * 5 / 9) + ' centigrade';
+        newStory = newStory.replace('150 pounds', weight);
+        newStory = newStory.replace('45 fahrenheit', temperature);
     }
 
     story.textContent = newStory;

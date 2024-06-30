3b6d20c8ff6f243e54e32dd121efbeb8162919ef
KaiHansen22 <166645695+KaiHansen22@users.noreply.github.com>
2024-06-30 12:15:57 -0600

    Update main.js

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -1,13 +1,14 @@
 document.getElementById('generate').addEventListener('click', generateStory);
 
 function generateStory() {
-    const storyText = 'It was a sunny day, and NAME decided to visit the local zoo. There, they encountered a talking parrot that shared some hilarious jokes! The jokes were often ';
+    const storyText = 'It was a sunny day, and NAME decided to visit the local zoo. There, they encountered a talking parrot that shared some hilarious jokes! The jokes were very  ';
     let newStory = storyText;
     const name = document.getElementById('customname').value;
-    const adjective = document.getElementById('adjective').value;
+    const adjective = document.getElementById('customadjective').value;
 
     if(name !== '') {
         newStory = newStory.replace('NAME', name);
+        newStory = newStory.replace('ADJECTIVE', adjective);
     } else {
         newStory = newStory.replace('NAME', 'Bob');
     }
